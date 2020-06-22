ad3ad0e4f188008d05c8401a4691dbcf5901b77d
Wesley Bortolozo <wesleyjr0101@gmail.com>
2020-06-22 18:41:27 -0300

    added vim lecture.md and exercises.md

--- Lecture03-Editors_VIM/fizbuzz.py
+++ Lecture03-Editors_VIM/fizbuzz.py
@@ -1,7 +1,7 @@
 import sys
 
 def fizz_buzz(limit):
-    for i in range(1, limit+1):
+    for i in range(1, limit+1)
         if i % 3 == 0:
             print('fizz', end='')
         if i % 5 == 0:
